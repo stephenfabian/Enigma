24b3b45249b0ee92ca3b518aa499208d857636ed
Stephen Fabian <fabianstephen@gmail.com>
2022-08-06 13:36:05 -0600

    refactor: .encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,8 +1,8 @@
-require 'date'
+  require 'date'
 
-class Enigma
+  class Enigma
 
-    def keys_hash(key)
+      def keys_hash(key)
         keys = Hash.new(0)
 
         keys[:a_key] = key[0..1].to_i
@@ -11,58 +11,71 @@ class Enigma
         keys[:d_key] = key[3..4].to_i
 
         keys
+      end
+
+      def offsets_hash(date)
+        offsets = Hash.new(0)
+        date_squared = date.to_i * date.to_i
+        last_four_digits_array = date_squared.digits.first(4).reverse
+
+        offsets[:a_offset] = last_four_digits_array[0]
+        offsets[:b_offset] = last_four_digits_array[1]
+        offsets[:c_offset] = last_four_digits_array[2]
+        offsets[:d_offset] = last_four_digits_array[3]
+
+        offsets
+      end 
+
+      def shifts_hash(key, date)
+        shifts = Hash.new(0)
+        shifts[:a_shift] = keys_hash(key)[:a_key] + offsets_hash(date)[:a_offset]
+        shifts[:b_shift] = keys_hash(key)[:b_key] + offsets_hash(date)[:b_offset]
+        shifts[:c_shift] = keys_hash(key)[:c_key] + offsets_hash(date)[:c_offset]
+        shifts[:d_shift] = keys_hash(key)[:d_key] + offsets_hash(date)[:d_offset]
+
+        shifts
+      end
+
+    def encrypt(*details)
+
+      encrypt_hash = Hash.new(0)
+      #MESSAGE
+      encrypt_hash[:encryption] = details[0]
+
+      #KEY -     #Keys A-D
+      if !details[1].nil? && details[1].size == 5 
+        encrypt_hash[:key] = details[1]
+      else
+        encrypt_hash[:key] = rand.to_s[2..6]
+      end
+
+      #DATE ->     #OFFSETS
+      if !details[2].nil? && details[2].size == 6 
+        encrypt_hash[:date] = details[2]
+      else
+          encrypt_hash[:date] = Date.today.strftime("%m%d%C")
+      end
+
+    #SHIFTS
+      alphabet_array = ("a".."z").to_a << " "
+      shifts = (shifts_hash(encrypt_hash[:key], encrypt_hash[:date])).values
+      message = encrypt_hash[:encryption].split("")
+      encrypted_msg = []
+  
+
+        message.each.with_index do |msg_character, index|
+          alphabet_array.each.with_index(alphabet_array.find_index(msg_character)) do |letter, index|
+            shifts.each do |shift|
+              if letter == msg_character
+                alphabet_array.rotate(shift)
+                encrypted_msg << alphabet_array[index]
+              end
+            end
+          end
+        end
+    require 'pry'; binding.pry
+  
+      encrypt_hash
     end
-
-    def offsets_hash(date)
-      offsets = Hash.new(0)
-      date_squared = date.to_i * date.to_i
-      last_four_digits_array = date_squared.digits.first(4).reverse
-
-      offsets[:a_offset] = last_four_digits_array[0]
-      offsets[:b_offset] = last_four_digits_array[1]
-      offsets[:c_offset] = last_four_digits_array[2]
-      offsets[:d_offset] = last_four_digits_array[3]
-
-      offsets
-    end 
-
-    def shifts_hash(key, date)
-      shifts = Hash.new(0)
-      shifts[:a_shift] = keys_hash(key)[:a_key] + offsets_hash(date)[:a_offset]
-      shifts[:b_shift] = keys_hash(key)[:b_key] + offsets_hash(date)[:b_offset]
-      shifts[:c_shift] = keys_hash(key)[:c_key] + offsets_hash(date)[:c_offset]
-      shifts[:d_shift] = keys_hash(key)[:d_key] + offsets_hash(date)[:d_offset]
-      
-      shifts
-    end
-
-  def encrypt(*details)
-
-    encrypt_hash = Hash.new(0)
-    #MESSAGE
-    encrypt_hash[:encryption] = details[0]
-    #-------------------------------------
-    #KEY - hash output
-    if !details[1].nil? && details[1].size == 5 
-      encrypt_hash[:key] = details[1]
-    else
-      encrypt_hash[:key] = rand.to_s[2..6]
-    end
-    #Keys A-D
-    keys_hash(encrypt_hash[:key])
-    #--------------------------------------------
-    #DATE
-    if !details[2].nil? && details[2].size == 6 
-      encrypt_hash[:date] = details[2]
-    else
-        encrypt_hash[:date] = Date.today.strftime("%m%d%C")
-    end
-    #--------------------------------------------
-#OFFSETS
-
-    offsets_hash(encrypt_hash[:date]) #helper
-    #--------------------------------------------
-    encrypt_hash
+  
   end
- 
-end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -65,7 +65,7 @@ describe Enigma do
     expect(enigma.keys_hash("02715")).to eq(expected)
   end
 
-  it 'can calculate each shift - key + offset' do
+  xit 'can calculate each shift - key + offset' do
     enigma = Enigma.new
     enigma.keys_hash("02715")
     enigma.offsets_hash("040895")
@@ -79,7 +79,7 @@ describe Enigma do
     expect(enigma.shifts_hash("02715", "040895")).to eq(expected)
   end
 
-  xit 'can encrypt a message' do
+  it 'can encrypt a message' do
     enigma = Enigma.new
     expected = {
                 encryption: "keder ohulw",
