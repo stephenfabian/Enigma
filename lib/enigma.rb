4640c0c504dc662719cad5f1dc50fd9fea300c23
Stephen Fabian <fabianstephen@gmail.com>
2022-08-06 19:52:53 -0600

    test: date_generator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -18,47 +18,44 @@
       end
     end
 
-      def keys_hash(key)
-        keys = Hash.new(0)
+    def keys_hash(key)
+      keys = Hash.new(0)
 
-        keys[:a_key] = key[0..1].to_i
-        keys[:b_key] = key[1..2].to_i
-        keys[:c_key] = key[2..3].to_i
-        keys[:d_key] = key[3..4].to_i
+      keys[:a_key] = key[0..1].to_i
+      keys[:b_key] = key[1..2].to_i
+      keys[:c_key] = key[2..3].to_i
+      keys[:d_key] = key[3..4].to_i
 
-        keys
-      end
+      keys
+    end
 
-      def offsets_hash(date)
-        offsets = Hash.new(0)
-        date_squared = date.to_i * date.to_i
-        last_four_digits_array = date_squared.digits.first(4).reverse
+    def offsets_hash(date)
+      offsets = Hash.new(0)
+      date_squared = date.to_i * date.to_i
+      last_four_digits_array = date_squared.digits.first(4).reverse
 
-        offsets[:a_offset] = last_four_digits_array[0]
-        offsets[:b_offset] = last_four_digits_array[1]
-        offsets[:c_offset] = last_four_digits_array[2]
-        offsets[:d_offset] = last_four_digits_array[3]
+      offsets[:a_offset] = last_four_digits_array[0]
+      offsets[:b_offset] = last_four_digits_array[1]
+      offsets[:c_offset] = last_four_digits_array[2]
+      offsets[:d_offset] = last_four_digits_array[3]
 
-        offsets
-      end 
+      offsets
+    end 
 
-      def shifts_hash(key, date)
-        shifts = Hash.new(0)
-        shifts[:a_shift] = keys_hash(key)[:a_key] + offsets_hash(date)[:a_offset]
-        shifts[:b_shift] = keys_hash(key)[:b_key] + offsets_hash(date)[:b_offset]
-        shifts[:c_shift] = keys_hash(key)[:c_key] + offsets_hash(date)[:c_offset]
-        shifts[:d_shift] = keys_hash(key)[:d_key] + offsets_hash(date)[:d_offset]
+    def shifts_hash(key, date)
+      shifts = Hash.new(0)
+      shifts[:a_shift] = keys_hash(key)[:a_key] + offsets_hash(date)[:a_offset]
+      shifts[:b_shift] = keys_hash(key)[:b_key] + offsets_hash(date)[:b_offset]
+      shifts[:c_shift] = keys_hash(key)[:c_key] + offsets_hash(date)[:c_offset]
+      shifts[:d_shift] = keys_hash(key)[:d_key] + offsets_hash(date)[:d_offset]
 
-        shifts
-      end
+      shifts
+    end
 
     def encrypt(*details)
 
-
       encrypt_hash = Hash.new(0)
       #MESSAGE
-      
-     #key (make into method?)
   
       #DATE -> (make into method)
       if !details[2].nil? && details[2].size == 6 
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,12 +7,21 @@ describe Enigma do
     expect(enigma).to be_an_instance_of(Enigma)
   end
 
-  it 'can generate the key - if key is not given, generate random one' do
+  xit 'can generate the key - if key is not given, generate random one' do
    enigma = Enigma.new
 
    expect(enigma.key_generator("hello world", "02715", "040895")).to eq("02715")
    expect(enigma.key_generator("hello world", "02715").length).to eq(5)
    expect(enigma.key_generator("hello world").length).to eq(5)
+  end
+
+  it 'can generate the date - if date is not given, generate todays date'  do
+    enigma = Enigma.new
+   
+    expect(enigma.date_generator("hello world", "02715", "040895")).to eq("040895")
+    expect(enigma.date_generator("hello world", "040895")).to eq("040895")
+    expect(enigma.date_generator("hello world")).to eq(Date.today.strftime("%m%d%C"))
+
   end
 
   
