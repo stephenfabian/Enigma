f19618d15e820277f7becc0c0d03f86ba4c448a8
Stephen Fabian <fabianstephen@gmail.com>
2022-08-09 14:14:15 -0600

    feat: self evaluation readme

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,24 +1,14 @@
 require './lib/enigma'
 
 handle = File.open(ARGV[0], "r") 
-
 incoming_text = handle.read
-
 handle.close
-
-
 enigma = Enigma.new
-
 encrypt = enigma.encrypt(incoming_text)
-
-
 writer = File.open(ARGV[1], "w")
-
 writer.write(encrypt[:encryption]) 
-
 writer.close
-
-puts "Encrypted text has been written to #{ARGV[1]}, the key is #{encrypt[:key]} and the date is #{encrypt[:date]}." 
+puts "Encrypted text has been written to #{ARGV[1]}, the key is #{encrypt[:key]} and the date is #{encrypt[:date]}" 
 
 
 
